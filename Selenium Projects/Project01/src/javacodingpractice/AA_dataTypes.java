fa9e6afa476784708c40d43643fc42941c307d00
Waqas <110913495+wahmed20@users.noreply.github.com>
2022-08-26 22:26:26 -0400

    upload Data Type concepts

---
+++ Selenium Projects/Project01/src/javacodingpractice/AA_dataTypes.java
@@ -0,0 +1,23 @@
+package javacodingpractice;
+
+public class AA_dataTypes {
+
+	public static void main(String[] args) 
+	{
+		// Primitive data types: int, double, boolean, char
+	// Data types
+// 1. integer 
+		int a = 10;
+// 2. Double
+		double b = 12.34;
+// 3. char. only single digit value. should be written within single quotes.
+		char c = 'd';
+// 4. boolean 
+		boolean b1 = true;
+		boolean b2 = false;
+// 5. String. is a class. not a data type but can be used as data type for string values
+		// it should be written in double quotes
+		String s = "Hello world";
+	}
+
+}
