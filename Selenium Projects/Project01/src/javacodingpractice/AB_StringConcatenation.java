3fd42a20b95b9272376f92ab15a947c538644142
Waqas <110913495+wahmed20@users.noreply.github.com>
2022-08-27 21:59:25 -0400

    String Concatenation

---
+++ Selenium Projects/Project01/src/javacodingpractice/AB_StringConcatenation.java
@@ -0,0 +1,18 @@
+package javacodingpractice;
+
+public class AB_StringConcatenation {
+
+	public static void main(String[] args) 
+	{
+		// + symbol is called concatenation operator 
+		int a = 100;
+		int b = 200;
+		String x = "Hello";
+		String y = "World";
+		System.out.println("the value of a is: " + a); // the value of a is: 100
+		System.out.println("the value of a is: " + b); // the value of a is: 200
+		System.out.println("the value of a is: " + a + b); //the value of a is: 100200
+		System.out.println("the value of a is: " + (a + b)); // the value of a is: 300
+	}
+
+}
